72fad0a75960deb629d3300a11966538d4736792
shhyatt <shhyatt@gmail.com>
2019-04-03 12:50:15 -0400

    modelsAndControllers

---
+++ app/controllers/api/v1/hikes_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::HikesController < ApplicationController
+end
---
+++ db/migrate/20190403163910_create_hikes.rb
@@ -0,0 +1,29 @@
+class CreateHikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :hikes do |t|
+      t.string :type
+      t.string :summary
+      t.string :difficulty
+      t.float :stars
+      t.string :starVotes
+      t.string :location
+      t.string :url
+      t.string :imgSqSmall
+      t.string :imgSmall
+      t.string :imgSmallMed
+      t.string :imgMedium
+      t.float :length
+      t.integer :ascent
+      t.integer :descent
+      t.integer :high
+      t.integer :low
+      t.float :longitude
+      t.float :latitude
+      t.string :conditionStatus
+      t.string :conditionDetails
+      t.string :conditionDate
+
+      t.timestamps
+    end
+  end
+end
